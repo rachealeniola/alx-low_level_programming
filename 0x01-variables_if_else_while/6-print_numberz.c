298f2d1e770888591e504ec914e1b3c9a1f28093
rachealeniola <rachealeniola92@gmail.com>
2022-09-11 10:19:26 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - print all single number using putchar
+ *
+ * Description: using main function
+ * Return: 0
+ */
+int main(void)
+{
+	int x;
+
+	for (x = 0; x < 10; x++)
+	{
+		putchar(x);
+	}
+	putchar('\n');
+	return (0);
+}
