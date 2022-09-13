30dedc82cfab6e7916fc316d57cda4a973e68be3
rachealeniola <rachealeniola92@gmail.com>
2022-09-13 14:54:25 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - prints _putchar, followed by a new line
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char s[] = "_putchar";
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		_putchar(s[i]);
+		i++;
+	}
+	_putchar(10);
+	return (0);
+}
