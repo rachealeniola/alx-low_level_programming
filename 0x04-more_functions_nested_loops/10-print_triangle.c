94f2911d2dfec6a668932c63a13c7b5bd1dbcfa1
rachealeniola <rachealeniola92@gmail.com>
2022-09-15 16:40:15 -0700

    task 10

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * print_triangle - display a triangele
+ * @size:integer
+ * Return: void
+ */
+
+void print_triangle(int size)
+{
+	int i, j, k;
+
+	if (size > 0)
+	{
+		for (i = 1; i <= size; i++)
+		{
+		for (j = 0; j <= (size - i - 1); j++)
+		{
+			_putchar(' ');
+		}
+		for (k = 0; k <= i - 1; k++)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
+		}
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
