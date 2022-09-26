019455ae53797222bbd3b69092a287948caefc07
rachealeniola <rachealeniola92@gmail.com>
2022-09-26 13:16:55 -0700

    task 2 locates a character in a string

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strchr - Locates a character in a string
+ * @s: string
+ * @c: char
+ * Return: If c is found - a pointer to the first occurence
+ *         If c is not found - NULL
+ */
+char *_strchr(char *s, char c)
+{
+	int y;
+
+	for (y = 0; s[y] >= '\0'; y++)
+	{
+		if (s[y] == c)
+			return (s + y);
+	}
+
+	return ('\0');
+}
