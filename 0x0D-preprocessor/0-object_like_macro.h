c0904d800c77da4cce5802aa84233fce8dc9aa5b
rachealeniola <rachealeniola92@gmail.com>
2022-10-10 20:32:50 -0700

    last one fix on task 0

--- 0x0D-preprocessor/0-object_like_macro.h
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -1,14 +1,3 @@
-#ifndef OBJECT_LIKE_MACRO_H
-#define OBJECT_LIKE_MACRO_H
-
-/**
- * file: 0-object_like_macro.h
- * Desc: header file that defines a macro named
- * SIZE as an abbreviation for the token 1024
- *
- */
-
 #ifndef SIZE
 #define SIZE 1024
-
 #endif
