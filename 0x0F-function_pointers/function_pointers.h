9ac986dcc20c7bffac323508a3084fbc760805f7
rachealeniola <rachealeniola92@gmail.com>
2022-10-12 06:01:33 -0700

    a functio that excutes array

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,9 +1,8 @@
 #ifndef _FUNCTION_FILE_H_
 #define _FUNCTION_FILE_H_
 
-#include <stdlib.h>
+#include <stdio.h>
 
-int _putchar(char c)
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
 int int_index(int *array, int size, int (*cmp)(int));
