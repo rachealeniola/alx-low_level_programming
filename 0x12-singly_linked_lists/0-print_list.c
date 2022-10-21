d88a01680b68a7b6e062f7b95d50fe1d72c82bbe
root <rachealeniola92@gmail.com>
2022-10-21 14:46:01 -0700

    tghdh

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -16,7 +16,7 @@ size_t print_list(const list_t *h)
 	{
 		if (h->str == NULL)
 			printf("[0] (nil)\n");
-		
+
 		else
 			printf("[%d] %s\n", h->len, h->str);
 
@@ -26,5 +26,3 @@ size_t print_list(const list_t *h)
 
 	return (nodes);
 }
-
-
